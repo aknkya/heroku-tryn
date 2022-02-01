e34c7dc7d309a303ea3f5c3cf6d02e5254e70fdd
akin <akn_kya@hotmail.com>
2022-02-01 13:18:16 +0300

    first commit

---
+++ src/main/java/com/example/herokuappdemo/HerokuappDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokuappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuappDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/herokuappdemo/HerokuappDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokuappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuappDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
