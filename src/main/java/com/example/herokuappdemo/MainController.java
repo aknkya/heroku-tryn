b23061242efefe4a3f2d56684dad6949c50e356f
akin <akn_kya@hotmail.com>
2022-02-02 14:46:39 +0300

    login screen update

--- src/main/java/com/example/herokuappdemo/MainController.java
+++ src/main/java/com/example/herokuappdemo/MainController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
     @GetMapping("/")
     public String getMainPage(){
 
-        return "Merhaba BASAK hacklendin";
+        return "index";
 
     }
 }
